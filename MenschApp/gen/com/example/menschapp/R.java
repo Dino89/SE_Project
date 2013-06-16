b3113b27cdf20aac26d50683f77b7ba5ad15e7b3
Malte Dauskardt <md656578@fh-muenster.de>
2013-06-16 15:49:33 +0200

    bin entfernt, bilder angepasst

--- MenschApp/gen/com/example/menschapp/R.java
+++ MenschApp/gen/com/example/menschapp/R.java
@@ -56,9 +56,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int way_up=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f0b0063;
-        public static final int create_game=0x7f0b0058;
-        public static final int email=0x7f0b005d;
+        public static final int action_settings=0x7f0b0064;
+        public static final int create_game=0x7f0b0059;
+        public static final int email=0x7f0b005e;
         public static final int field_1=0x7f0b001e;
         public static final int field_10=0x7f0b0027;
         public static final int field_11=0x7f0b0028;
@@ -131,29 +131,30 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int field_yellow_house_2=0x7f0b0047;
         public static final int field_yellow_house_3=0x7f0b0048;
         public static final int field_yellow_house_4=0x7f0b0049;
-        public static final int highscore=0x7f0b0057;
-        public static final int itemPrefs=0x7f0b0062;
+        public static final int highscore=0x7f0b0058;
+        public static final int itemPrefs=0x7f0b0063;
         public static final int listView1=0x7f0b0002;
-        public static final int lobbylist=0x7f0b0056;
-        public static final int login=0x7f0b005f;
-        public static final int login_form=0x7f0b005c;
-        public static final int login_status=0x7f0b005a;
-        public static final int login_status_message=0x7f0b005b;
+        public static final int lobbylist=0x7f0b0057;
+        public static final int login=0x7f0b0060;
+        public static final int login_form=0x7f0b005d;
+        public static final int login_status=0x7f0b005b;
+        public static final int login_status_message=0x7f0b005c;
         public static final int mitspielen=0x7f0b0008;
         public static final int mitspieler=0x7f0b0003;
-        public static final int password=0x7f0b005e;
-        public static final int register_button=0x7f0b0061;
-        public static final int sign_in_button=0x7f0b0060;
+        public static final int password=0x7f0b005f;
+        public static final int register_button=0x7f0b0062;
+        public static final int sign_in_button=0x7f0b0061;
         public static final int spieler1=0x7f0b0004;
         public static final int spieler2=0x7f0b0005;
         public static final int spieler3=0x7f0b0006;
         public static final int spieler4=0x7f0b0007;
         public static final int spielleiter=0x7f0b0001;
-        public static final int update_pic=0x7f0b0059;
+        public static final int update_pic=0x7f0b005a;
         public static final int way_down=0x7f0b0053;
         public static final int way_left=0x7f0b0054;
         public static final int way_right=0x7f0b0052;
         public static final int way_up=0x7f0b0055;
+        public static final int wuerfeln=0x7f0b0056;
         public static final int zuschauen=0x7f0b0009;
         public static final int zuschauer=0x7f0b0000;
     }
@@ -170,61 +171,61 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int login=0x7f0a0003;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f07001f;
-        public static final int action_register=0x7f07001d;
+        public static final int action_forgot_password=0x7f070020;
+        public static final int action_register=0x7f07001e;
         public static final int action_settings=0x7f070004;
-        public static final int action_sign_in_register=0x7f07001c;
-        public static final int action_sign_in_short=0x7f07001e;
-        public static final int action_store_login_credentials=0x7f070027;
+        public static final int action_sign_in_register=0x7f07001d;
+        public static final int action_sign_in_short=0x7f07001f;
+        public static final int action_store_login_credentials=0x7f070028;
         public static final int app_name=0x7f070000;
         public static final int create_game=0x7f07000b;
         public static final int dummy_button=0x7f070001;
         public static final int dummy_content=0x7f070002;
-        public static final int error_field_required=0x7f070025;
-        public static final int error_incorrect_password=0x7f070024;
-        public static final int error_invalid_email=0x7f070022;
-        public static final int error_invalid_password=0x7f070023;
-        public static final int error_register=0x7f070026;
+        public static final int error_field_required=0x7f070026;
+        public static final int error_incorrect_password=0x7f070025;
+        public static final int error_invalid_email=0x7f070023;
+        public static final int error_invalid_password=0x7f070024;
+        public static final int error_register=0x7f070027;
         public static final int field=0x7f070014;
         public static final int field_h=0x7f070013;
         public static final int field_start=0x7f070015;
         public static final int hello_world=0x7f070005;
-        public static final int highscore=0x7f070018;
+        public static final int highscore=0x7f070019;
         public static final int labelPrefs=0x7f070008;
-        public static final int login_progress_signing_in=0x7f070020;
-        public static final int mitspielen=0x7f070017;
-        public static final int pref_default_display_name=0x7f070031;
-        public static final int pref_description_social_recommendations=0x7f07002f;
+        public static final int login_progress_signing_in=0x7f070021;
+        public static final int mitspielen=0x7f070018;
+        public static final int pref_default_display_name=0x7f070032;
+        public static final int pref_description_social_recommendations=0x7f070030;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f070033;
+        public static final int pref_header_data_sync=0x7f070034;
         /**  Example General settings 
          */
-        public static final int pref_header_general=0x7f07002d;
+        public static final int pref_header_general=0x7f07002e;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f070036;
-        public static final int pref_ringtone_silent=0x7f070039;
-        public static final int pref_title_add_friends_to_messages=0x7f070032;
-        public static final int pref_title_display_name=0x7f070030;
-        public static final int pref_title_new_message_notifications=0x7f070037;
-        public static final int pref_title_ringtone=0x7f070038;
-        public static final int pref_title_social_recommendations=0x7f07002e;
-        public static final int pref_title_sync_frequency=0x7f070034;
-        public static final int pref_title_system_sync_settings=0x7f070035;
-        public static final int pref_title_vibrate=0x7f07003a;
+        public static final int pref_header_notifications=0x7f070037;
+        public static final int pref_ringtone_silent=0x7f07003a;
+        public static final int pref_title_add_friends_to_messages=0x7f070033;
+        public static final int pref_title_display_name=0x7f070031;
+        public static final int pref_title_new_message_notifications=0x7f070038;
+        public static final int pref_title_ringtone=0x7f070039;
+        public static final int pref_title_social_recommendations=0x7f07002f;
+        public static final int pref_title_sync_frequency=0x7f070035;
+        public static final int pref_title_system_sync_settings=0x7f070036;
+        public static final int pref_title_vibrate=0x7f07003b;
         /**  Strings related to Settings 
   LOGIN STRINGS 
          */
-        public static final int pref_user_name=0x7f070029;
-        public static final int pref_user_name_summary=0x7f07002a;
-        public static final int pref_user_password=0x7f07002b;
-        public static final int pref_user_password_summary=0x7f07002c;
+        public static final int pref_user_name=0x7f07002a;
+        public static final int pref_user_name_summary=0x7f07002b;
+        public static final int pref_user_password=0x7f07002c;
+        public static final int pref_user_password_summary=0x7f07002d;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f07001a;
-        public static final int prompt_password=0x7f07001b;
-        public static final int register_progress_signing_in=0x7f070021;
+        public static final int prompt_email=0x7f07001b;
+        public static final int prompt_password=0x7f07001c;
+        public static final int register_progress_signing_in=0x7f070022;
         public static final int settings=0x7f070006;
         public static final int spieler=0x7f07000e;
         public static final int spieler1=0x7f07000f;
@@ -235,10 +236,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int title_activity_game_detail=0x7f070007;
         public static final int title_activity_game_field=0x7f070009;
         public static final int title_activity_lobby=0x7f070003;
-        public static final int title_activity_login=0x7f070019;
-        public static final int title_activity_settings=0x7f070028;
+        public static final int title_activity_login=0x7f07001a;
+        public static final int title_activity_settings=0x7f070029;
         public static final int update_gamelist=0x7f07000a;
-        public static final int zuschauen=0x7f070016;
+        public static final int wuerfeln=0x7f070016;
+        public static final int zuschauen=0x7f070017;
         public static final int zuschauer=0x7f07000d;
     }
     public static final class style {
